433bda5a43cbb58c772f58bc942b5a15083f88cd
lexa <sokolov@ozero.net>
2018-04-09 23:17:24 +0300

    Constructor created

--- src/main/java/Queue.java
+++ src/main/java/Queue.java
@@ -1,7 +1,13 @@
 import java.util.LinkedList;
 
 public class Queue {
-    LinkedList queue = new LinkedList();
+    LinkedList queue;
+
+    public Queue() {
+        queue = new LinkedList();
+    }
+
+
 
     public static void main(String[] args) {
         System.out.println("hello");
