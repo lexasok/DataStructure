f6cad80dac25ac11f311f0da2efef2ece8a66829
lexa <sokolov@ozero.net>
2018-04-09 23:14:41 +0300

    Class renamed, field created

---
+++ src/main/java/Queue.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Queue {
+    LinkedList queue = new LinkedList();
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
