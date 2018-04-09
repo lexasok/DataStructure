b9ec3b27d77afb20d24dfd376dff5f5f0c8022b0
lexa <sokolov@ozero.net>
2018-04-10 00:09:39 +0300

    Data input reading done

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        char[] chars = scan.nextLine().toCharArray();
+        scan.close();
+
+
+    }
 }
